31d1e0a66e9c0a321f214b555c6dc5e89ae8bcf8
Mirai Hattori <mirai2045@gmail.com>
2026-05-29 07:36:18 +0000

    [Example] Use UR5e arm for palletizing

--- examples/rigid/single_sku_palletizing_amd.py
+++ examples/rigid/single_sku_palletizing_amd.py
@@ -12,7 +12,6 @@ PICK_QUAT = np.array([0.0, 1.0, 0.0, 0.0])
 SUCTION_CUP_HEIGHT = 0.025
 SUCTION_CUP_RADIUS = 0.035
 ROBOT_TOOL_OFFSET = np.array([0.0, 0.0, 0.045])
-FINGER_OPEN = np.array([0.04, 0.04])
 
 
 def parse_args():
@@ -56,7 +55,6 @@ def move_vacuum_cup(
     target,
     steps,
     motors_dof,
-    fingers_dof=None,
     attached_carton=None,
 ):
     start = entity_pos(cup)
@@ -70,8 +68,6 @@ def move_vacuum_cup(
 
         qpos = robot.inverse_kinematics(link=end_effector, pos=pos + ROBOT_TOOL_OFFSET, quat=PICK_QUAT)
         robot.control_dofs_position(qpos[motors_dof], motors_dof)
-        if fingers_dof is not None:
-            robot.control_dofs_position(FINGER_OPEN, fingers_dof)
         scene.step()
 
 
@@ -104,15 +100,15 @@ def suction_sealed(cup, carton, xy_tolerance=0.018, z_tolerance=0.018):
     return xy_error <= xy_tolerance and z_error <= z_tolerance
 
 
-def attach_with_suction(scene, cup, robot, end_effector, carton, motors_dof, fingers_dof, travel_steps, settle_steps):
+def attach_with_suction(scene, cup, robot, end_effector, carton, motors_dof, travel_steps, settle_steps):
     top = carton_top_center(carton)
     hover = top + np.array([0.0, 0.0, 0.16 + SUCTION_CUP_HEIGHT * 0.5])
-    move_vacuum_cup(scene, cup, robot, end_effector, hover, travel_steps, motors_dof, fingers_dof)
+    move_vacuum_cup(scene, cup, robot, end_effector, hover, travel_steps, motors_dof)
 
     for _ in range(4):
         top = carton_top_center(carton)
         seal = top + np.array([0.0, 0.0, SUCTION_CUP_HEIGHT * 0.5])
-        move_vacuum_cup(scene, cup, robot, end_effector, seal, settle_steps, motors_dof, fingers_dof)
+        move_vacuum_cup(scene, cup, robot, end_effector, seal, settle_steps, motors_dof)
         if suction_sealed(cup, carton):
             wait(scene, settle_steps)
             return
@@ -197,19 +193,18 @@ def main():
         ),
         surface=gs.surfaces.Plastic(color=(0.02, 0.08, 0.10)),
     )
-    robot = scene.add_entity(gs.morphs.MJCF(file="xml/franka_emika_panda/panda.xml"))
+    robot = scene.add_entity(gs.morphs.MJCF(file="xml/universal_robots_ur5e/ur5e.xml"))
     scene.build()
 
-    motors_dof = np.arange(7)
-    fingers_dof = np.arange(7, 9)
-    end_effector = robot.get_link("hand")
-    robot.set_dofs_kp(np.array([4500, 4500, 3500, 3500, 2000, 2000, 2000, 120, 120]))
-    robot.set_dofs_kv(np.array([450, 450, 350, 350, 200, 200, 200, 12, 12]))
+    motors_dof = np.arange(6)
+    end_effector = robot.get_link("ee_virtual_link")
+    robot.set_dofs_kp(np.array([2200, 2200, 1800, 700, 700, 500]))
+    robot.set_dofs_kv(np.array([120, 120, 100, 35, 35, 25]))
     robot.set_dofs_force_range(
-        np.array([-87, -87, -87, -87, -12, -12, -12, -100, -100]),
-        np.array([87, 87, 87, 87, 12, 12, 12, 100, 100]),
+        np.array([-150, -150, -150, -28, -28, -28]),
+        np.array([150, 150, 150, 28, 28, 28]),
     )
-    robot.set_dofs_position(np.array([-0.65, 0.9, 0.65, -1.65, -0.75, 1.45, 0.55, *FINGER_OPEN]))
+    robot.set_dofs_position(np.array([-1.2, -1.35, 1.65, -1.85, -1.55, 0.0]))
     wait(scene, 80 if not args.fast else 8)
 
     travel_steps = 90 if not args.fast else 35
@@ -228,17 +223,16 @@ def main():
             end_effector,
             carton,
             motors_dof,
-            fingers_dof,
             travel_steps,
             settle_steps,
         )
 
-        move_vacuum_cup(scene, vacuum_cup, robot, end_effector, hover_pick, travel_steps, motors_dof, fingers_dof, carton)
-        move_vacuum_cup(scene, vacuum_cup, robot, end_effector, hover_place_cup, travel_steps, motors_dof, fingers_dof, carton)
-        move_vacuum_cup(scene, vacuum_cup, robot, end_effector, release_cup, settle_steps, motors_dof, fingers_dof, carton)
+        move_vacuum_cup(scene, vacuum_cup, robot, end_effector, hover_pick, travel_steps, motors_dof, carton)
+        move_vacuum_cup(scene, vacuum_cup, robot, end_effector, hover_place_cup, travel_steps, motors_dof, carton)
+        move_vacuum_cup(scene, vacuum_cup, robot, end_effector, release_cup, settle_steps, motors_dof, carton)
         wait(scene, settle_steps)
         wait(scene, settle_steps)
-        move_vacuum_cup(scene, vacuum_cup, robot, end_effector, hover_place, settle_steps, motors_dof, fingers_dof)
+        move_vacuum_cup(scene, vacuum_cup, robot, end_effector, hover_place, settle_steps, motors_dof)
 
     print(f"Completed single-SKU palletizing for {requested_cartons} carton(s).")
 
